be5a14e675953abf1a9b314d2a0de6d2430f5d65
Rajat Kanti Bhattacharjee <rajatthepagal@gmail.com>
2018-10-21 16:49:52 +0530

    Changed the damn perceptual loss network , added reflection padding this gets rid of frames appearing hopefully, next is to add total variational loss, but that is just for image smoothening

--- Perceptual Loss Style transfer - rajat/main.py
+++ Perceptual Loss Style transfer - rajat/main.py
@@ -7,6 +7,7 @@ from keras.layers.advanced_activations import *
 from keras import metrics
 from keras.applications import *
 from keras.preprocessing import image
+import tensorflow as tf
 
 import numpy as np
 import os
@@ -29,18 +30,25 @@ class Fast_Style_transfer:
 
     def residual_block(self , x , filters = 128):
         inp = x
-        x = Conv2D(filters= filters , kernel_size=(3,3) , strides=(1,1) , padding='same',kernel_initializer='glorot_uniform')(x)
+        x = Lambda(lambda x: tf.pad(x , [[0,0],[1,1],[1,1],[0,0]], 'REFLECT'))(x)
+        x = Conv2D(filters= filters , kernel_size=(3,3) , strides=(1,1) , padding='valid',kernel_initializer='glorot_uniform')(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
-        x = Conv2D(filters= filters , kernel_size=(3,3) , strides=(1,1) , padding='same',kernel_initializer='glorot_uniform')(x)
+        x = Lambda(lambda x: tf.pad(x , [[0,0],[1,1],[1,1],[0,0]], 'REFLECT'))(x)
+        x = Conv2D(filters= filters , kernel_size=(3,3) , strides=(1,1) , padding='valid',kernel_initializer='glorot_uniform')(x)
         x = BatchNormalization()(x)
         x = Add()([inp , x])
         return x
 
-    def Convolution_block(self , x ,filters ,  kernel_size , strides , padding , activation = 'relu'):
+    def Convolution_block(self , x ,filters ,  kernel_size , strides , activation = 'relu'):
         # Use same padding when you are using stride greater than 1. 
         # The Calculation is not messed up then and you get expected block size
-        x = Conv2D(filters= filters , kernel_size=kernel_size , strides=strides , padding=padding,kernel_initializer='glorot_uniform')(x)
+        p = ( kernel_size[0]-1 ) // 2
+        if strides[0] == 1:
+            x = Lambda(lambda x: tf.pad(x , [[0,0],[p,p],[p,p],[0,0]], 'REFLECT'))(x)
+            x = Conv2D(filters= filters , kernel_size=kernel_size , strides=strides , padding='valid',kernel_initializer='glorot_uniform')(x)
+        else:
+            x = Conv2D(filters= filters , kernel_size=kernel_size , strides=strides , padding='same',kernel_initializer='glorot_uniform')(x)            
         x = BatchNormalization()(x)
         x = Activation(activation)(x)
         return x
@@ -48,8 +56,8 @@ class Fast_Style_transfer:
     def Convolution_transpose_block(self, x , filters ,  kernel_size , strides ):
         # Use same padding when you are using stride greater than 1. 
         # The Calculation is not messed up then and you get expected block size
-        x = UpSampling2D(size=strides)(x)
-        x = Conv2D(filters=filters, kernel_size=kernel_size , strides=(1,1) , padding='same' , kernel_initializer='glorot_uniform')(x) 
+        # x = UpSampling2D(size=strides)(x)
+        x = Conv2DTranspose(filters=filters, kernel_size=kernel_size , strides=(2,2) , padding='same' , kernel_initializer='glorot_uniform')(x) 
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
         return x
@@ -61,7 +69,12 @@ class Fast_Style_transfer:
         ## Content loss
         self.content_loss_layer = 'block3_conv3'
         ## Texture loss
-        self.texture_loss_layers = ['block1_conv2' , 'block2_conv2' , 'block3_conv3' , 'block4_conv3']
+        self.texture_loss_layers = [
+                                    'block1_conv2' , 
+                                    'block2_conv2' , 
+                                    'block3_conv3' , 
+                                    'block4_conv3'
+                                    ]
 
         content_loss_output = [vgg.get_layer(self.content_loss_layer).output]
 
@@ -101,41 +114,53 @@ class Fast_Style_transfer:
         activations = args
         return K.constant(self.lambda_texture) * Add()(activations)
 
+    def vgg_preprocess(self , image_tensor):
+        ## VGG Preprocessing
+        gen_model_output_vgg = Lambda(lambda x: K.reverse(x,axes=2))(image_tensor)
+        # gen_model_output_vgg = Lambda(lambda x: x*127.5 + 127.5 , name='vgg_scaling')(gen_model_output_vgg)
+        gen_model_output_vgg = Lambda(lambda x: K.bias_add(x ,K.constant(-np.array([ 103.939, 116.779,123.68], dtype=np.float32))))(gen_model_output_vgg)
+        ## VGG Preprocessing
+        return gen_model_output_vgg
+
     def loss_model(self ,generator_input, generator_output , shape, content_image , texture_image):
         
         vgg = self.get_vgg_loss_model(shape)
 
         content_image = self.set_constant_image(content_image,shape)
-        texture_image = self.set_constant_image(texture_image,shape)
+        content_image_preprocessed = self.vgg_preprocess(content_image)
 
-        content_activations = vgg(content_image)
-        texture_activations = vgg(texture_image)
-        predicted_activations = vgg(generator_output) 
+        texture_image = self.set_constant_image(texture_image,shape)
+        texture_image_preprocessed = self.vgg_preprocess(texture_image)
+        predicted_image_preprocessed = self.vgg_preprocess(generator_output)
+
+        content_activations = vgg(content_image_preprocessed)
+        texture_activations = vgg(texture_image_preprocessed)
+        predicted_activations = vgg(predicted_image_preprocessed) 
 
         content_loss = Lambda(self.get_content_loss,output_shape=(1,), name='content_loss')([predicted_activations[0], content_activations[0]])
 
         texture_losses = []
-        for i in range(len(texture_activations)):
+        for i in range(1,len(texture_activations)):
             texture_losses.append(Lambda(self.get_texture_loss , output_shape=(1,) , name='texture_loss'+str(i))([predicted_activations[i], texture_activations[i]]))
 
         texture_loss = Lambda(self.total_texture_loss , output_shape=(1,) , name='total_texture_loss')(texture_losses)
 
-        model = Model(inputs=[generator_input,content_image,texture_image], outputs =[content_loss, texture_loss])
+        model = Model(inputs=[generator_input,content_image,texture_image], outputs =[generator_output, content_loss, texture_loss])
 
         return model
 
-    def vgg_mean_sub(self , x):
-        return K.bias_add(x ,K.constant(-np.array([123.68, 116.779, 103.939], dtype=np.float32)))
 
-    def __init__(self ,content_image, texture_image, shape=(256,256,3) , lr=0.0001 ,chk = -1 , lambda_tv=0, lambda_content = 0 , lambda_texture = 0 , test=False):
+    def __init__(self ,content_image, texture_image, test_folder , shape=(256,256,3) , lr=0.0001 ,chk = -1 , lambda_tv=0, lambda_content = 0 , lambda_texture = 0 , test=False):
         
+        self.checkpoint = chk
         self.lambda_content = lambda_content
         self.lambda_texture = lambda_texture
+        self.test_folder = test_folder
 
         inp = Input(shape , name='network_input')
-        x = self.Convolution_block(inp , filters =32 , kernel_size=(9,9) , strides=(1,1) , padding='same')
-        x = self.Convolution_block(x , filters =64 , kernel_size=(3,3) , strides=(2,2) , padding='same')
-        x = self.Convolution_block(x , filters =128 , kernel_size=(3,3) , strides=(2,2), padding='same')
+        x = self.Convolution_block(inp , filters =32 , kernel_size=(9,9) , strides=(1,1) )
+        x = self.Convolution_block(x , filters =64 , kernel_size=(3,3) , strides=(2,2) )
+        x = self.Convolution_block(x , filters =128 , kernel_size=(3,3) , strides=(2,2) )
         x = self.residual_block(x , filters=128)
         x = self.residual_block(x , filters=128)
         x = self.residual_block(x , filters=128)
@@ -143,7 +168,8 @@ class Fast_Style_transfer:
         x = self.residual_block(x , filters=128)
         x = self.Convolution_transpose_block(x , filters=64 , kernel_size=(3,3) , strides=(2,2))
         x = self.Convolution_transpose_block(x , filters=32 , kernel_size=(3,3) , strides=(2,2))
-        out = self.Convolution_block(x , filters=3 , kernel_size=(9,9) , strides=(1,1) ,padding='same')
+        x = self.Convolution_block(x , filters=3 , kernel_size=(9,9) , strides=(1,1) , activation='tanh')
+        out = Lambda(lambda x: x*127.5 + 127.5 , name='scaling_output')(x)
         gen_model = Model(inputs = inp , outputs = out)
         
         self.gen_model = gen_model
@@ -152,20 +178,18 @@ class Fast_Style_transfer:
 
         if not test:
             
-            ## VGG Preprocessing
-            gen_model_output_vgg = Lambda(lambda x: K.reverse(x,axes=2) , name='flip_axis')(gen_model.output)
-            gen_model_output_vgg = Lambda(lambda x: x * K.constant(255) , name='denormalise')(gen_model_output_vgg)
-            gen_model_output_vgg = Lambda(self.vgg_mean_sub , name='vgg_normalise')(gen_model_output_vgg)
-            ## VGG Preprocessing
-
-            self.loss_model = self.loss_model(gen_model.input,gen_model_output_vgg,shape ,content_image , texture_image )
+            self.loss_model = self.loss_model(gen_model.input,gen_model.output,shape ,content_image , texture_image )
             self.loss_model.summary()
             adam = Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=None)
-            self.loss_model.compile(loss={'content_loss':'mae' , 'total_texture_loss':'mae'}, optimizer = adam)
+            self.loss_model.compile(loss={'content_loss':'mae' , 'total_texture_loss':'mae' , 'scaling_output':'mse'}, optimizer = adam)
+            self.gen_model.compile(loss='mae' , optimizer = adam)
+        
+        if chk > -1:
+            self.gen_model.load_weights(test_folder+'/model_epoch'+str(chk)+'.h5')
 
     def gen_sample(self,x,test_folder, epoch):
-        generated_images = self.gen_model.predict(np.array([x])/255) # send in normalised
-        img = cv2.cvtColor(generated_images[0]*255 , cv2.COLOR_BGR2RGB) # get denormalised result
+        generated_images = self.gen_model.predict(np.array([x])) # send in normalised
+        img = cv2.cvtColor(generated_images[0] , cv2.COLOR_BGR2RGB) # get denormalised result
         cv2.imwrite(test_folder+'/result'+str(epoch)+'.jpg',img)
 
     def generate_data(self , data_location):
@@ -173,19 +197,27 @@ class Fast_Style_transfer:
         for batch in batches:
             yield np.load(data_location+"/"+batch)
 
-    def train(self, data_location ,test_folder, epochs  , batch_size):
-        for epoch in range(epochs):
+    def train(self, data_location , epochs  , batch_size):
+        if self.checkpoint < 0:
+            self.checkpoint = 0
+        test_folder = self.test_folder
+        for epoch in range(self.checkpoint,epochs):
+            print("log: epoch running:",epoch)
             for batch in self.generate_data(data_location):
-                zero = np.zeros(batch.shape[0])
-
-                ## send in normaised data
-                norm_batch = batch/255
-                self.loss_model.fit(x = norm_batch , y = {'content_loss':zero , 'total_texture_loss':zero} ,  batch_size = batch_size , verbose =1 , nb_epoch=1)
                 
-                img = cv2.cvtColor(batch[40] , cv2.COLOR_BGR2RGB)
+                if epoch == 0:
+                    print("log: fitting network to generate images !! epoch:",epoch)
+                    self.gen_model.fit(x = batch , y = batch , batch_size=4 , verbose = 1 , nb_epoch=1)
+
+                if epoch > 0:
+                    zero = np.zeros(batch.shape[0])
+                    print("log: fitting network for style and content")
+                    self.loss_model.fit(x = batch , y = {'content_loss':zero , 'total_texture_loss':zero , 'scaling_output':batch} ,  batch_size = batch_size , verbose =1 , nb_epoch=1)
+                
+                img = cv2.cvtColor(batch[100] , cv2.COLOR_BGR2RGB)
                 cv2.imwrite(test_folder+'/test_image.jpg',img)
                 
-                self.gen_sample(batch[40] , test_folder, epoch)
+                self.gen_sample(batch[100] , test_folder, epoch)
                 self.gen_model.save_weights(test_folder+'/model_epoch'+str(epoch)+'.h5')
 
 
@@ -224,6 +256,6 @@ if __name__=='__main__':
         print("log: Test folder exist")
     # data = data[:-(data.shape[0] % batch_size)]
     # print("Data shape : ",data.shape)
-    model = Fast_Style_transfer(shape=(256,256,3) , lr=learning_rate ,chk = chk , lambda_tv=0, lambda_content = lambda_content , lambda_texture = lambda_texture ,content_image = content_image, texture_image = texture_image, test=False)
-    model.train(data_location , test_folder,  epoch , batch_size)
+    model = Fast_Style_transfer(shape=(256,256,3) , lr=learning_rate ,chk = chk , lambda_tv=0, lambda_content = lambda_content , lambda_texture = lambda_texture ,content_image = content_image, texture_image = texture_image, test_folder=test_folder ,test=False)
+    model.train(data_location ,  epoch , batch_size)
     
