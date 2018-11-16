dfd9a3fa51c8a2c1524a697828ad22c793b205ca
Amrit Jha <amritjha107@gmail.com>
2018-11-16 17:56:55 +0530

    a little more progress - ui

--- ui - amrit/project/src/app/app.module.ts
+++ ui - amrit/project/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +19,7 @@ import { LandingComponent } from './landing/landing.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
