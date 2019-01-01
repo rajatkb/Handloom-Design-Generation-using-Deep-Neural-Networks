cbe865f051e3cdfc516e3bfafb52742f40f63f60
Rajat Kanti Bhattacharjee <rajatthepagal@gmail.com>
2019-01-01 12:50:20 +0530

    app commited and few directort structure change

---
+++ preliminary work/ui - amrit/project/src/app/directives/filedrop.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FiledropDirective } from './filedrop.directive';
+
+describe('FiledropDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FiledropDirective();
+    expect(directive).toBeTruthy();
+  });
+});
